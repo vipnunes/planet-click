c42c352f3de1d88b1c3f7e45a3d40cf550e5fe83
Vipnunes <otavio@vipnunes.com>
2024-02-22 18:26:01 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,75 +6,75 @@ var bitcoinRate = 0
 var items = [
   {
     "name": "item_oldCalculator",
-    "price": "0.0000001"
+    "price": "1"
   },
   {
     "name": "item_oldCpu",
-    "price": "0.00000125"
+    "price": "25"
   },
   {
     "name": "item_oldComputerFromGrandpa",
-    "price": "0.00003"
+    "price": "50"
   },
   {
     "name": "item_rapsberrypy",
-    "price": "0.00005"
+    "price": "75"
   },
   {
     "name": "item_smartphone",
-    "price": "0.0005"
+    "price": "100"
   },
   {
     "name": "item_middleClassPC",
-    "price": "0.0015"
-  },
-  {
-    "name": "item_cheapServer",
-    "price": "0.004"
-  },
-  {
-    "name": "item_gamingPC",
-    "price": "0.015"
-  },
-  {
-    "name": "item_cheapMiner",
-    "price": "0.05"
-  },
-  {
-    "name": "item_highEndUltraPC",
-    "price": "0.15"
-  },
-  {
-    "name": "item_bigMiner",
-    "price": "1.5"
-  },
-  {
-    "name": "item_miningFarm",
     "price": "250"
   },
   {
+    "name": "item_cheapServer",
+    "price": "500"
+  },
+  {
+    "name": "item_gamingPC",
+    "price": "750"
+  },
+  {
+    "name": "item_cheapMiner",
+    "price": "1000"
+  },
+  {
+    "name": "item_highEndUltraPC",
+    "price": "1250"
+  },
+  {
+    "name": "item_bigMiner",
+    "price": "1500"
+  },
+  {
+    "name": "item_miningFarm",
+    "price": "1750"
+  },
+  {
     "name": "item_nasaPC",
-    "price": "5000"
+    "price": "2000"
   },
   {
     "name": "item_quantumRig",
-    "price": "245000"
+    "price": "5000"
   },
   {
     "name": "item_miningFarmSpace",
-    "price": "2000000"
+    "price": "10000"
   },
   {
     "name": "item_miningFarmMoon",
-    "price": "75500000"
+    "price": "50000"
   },
   {
     "name": "item_bitcoinTimeMachine",
-    "price": "975000000"
+    "price": "100000"
   },
   {
     "name": "item_blackHolePoweredMiner",
-    "price": "750000000000"
+    "price": "1000000"
   }
 ]
 
@@ -444,7 +444,7 @@ $(document).ready(function () {
   $(".bitcoin").click(function () {
 
     // Add 1^-8 Bitcoins (equal to 1 satoshi)
-    bitcoins = bitcoins + 0.00000001
+    bitcoins = bitcoins + 1
 
     // Show the new number on the page
     if(bitcoins > 1000000){
